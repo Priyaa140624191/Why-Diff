322b42bb023eb0d6e8177842ee2fe521b32dfc6b
Priyaa140624191 <P.Thavasimani2@newcastle.ac.uk>
2017-05-11 18:44:15 +0100

    Updated TryFT

--- WhyDiffAlgorithm/src/main/java/Traversal/WhyDiffAlgorithm/TryFT.java
+++ WhyDiffAlgorithm/src/main/java/Traversal/WhyDiffAlgorithm/TryFT.java
@@ -2,8 +2,6 @@ package Traversal.WhyDiffAlgorithm;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.DynamicRelationshipType;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
@@ -66,25 +64,8 @@ public class TryFT {
 					if (!firstId.contains(checkEntityVisited)) {
 						addVisited.add(checkEntityVisited);
 					}
-
-					/*
-					 * Node NewEntity = graphDB.findNode(NodeType1.entity, "id",
-					 * checkEntityVisited);
-					 * System.out.println("New Entity Id"+NewEntity.getId());
-					 */
-					// returnedActivity = checkEntity(NewEntity);
-					// }
-
 				}
 				addVisited.remove(addVisited.size() - 1);
-
-				/*
-				 * 
-				 * System.out.println("List Display " +addVisited.toString());
-				 * if (!addVisited.isEmpty()) { firstId =
-				 * addVisited.get(0).toString(); firstEntity =
-				 * graphDB.findNode(NodeType1.entity, "id", firstId); } else {
-				 */
 
 				firstId = returnedEntity;
 				firstEntity = graphDB.findNode(NodeType1.entity, "id", firstId);
@@ -126,62 +107,69 @@ public class TryFT {
 		} catch (Exception ex) {
 
 		}
-		 long stopTime = System.currentTimeMillis();
-	     long elapsedTime = stopTime - startTime;
-	     System.out.println(elapsedTime);
+		long stopTime = System.currentTimeMillis();
+		long elapsedTime = stopTime - startTime;
+		System.out.println(elapsedTime);
 	}
 
 	private static void checkUnvisitedNodes(List<String> addVisited,
 			List<String> addVisited1) {
 		System.out.println("Display unvisted" + addVisited.toString());
 		System.out.println("Display unvisted 1" + addVisited1.toString());
-		System.out.println("try");
-		String DisplayId = addVisited.get(0);
-		String DisplayId2 = addVisited1.get(0);
-		// System.out.println("Display Id 1"+addVisited1.get(0));
-		try {
-			ResourceIterator<Node> missedentity1 = graphDB.findNodes(
-					NodeType1.entity, "id", DisplayId);
-			ResourceIterator<Node> missedentity2 = graphDB.findNodes(
-					NodeType1.entity, "id", DisplayId2);
-			Node missedFirstEntity = missedentity1.next();
-			Node missedSecondEntity = missedentity2.next();
-			
-			System.out.println("Comparing Entities "+(String)missedFirstEntity.getProperty("id") +
-					"\t\t"+ (String)missedSecondEntity.getProperty("id"));
-			compare((String) missedFirstEntity.getProperty("attributes"),
-					(String) missedSecondEntity.getProperty("attributes"));
-			
-			String activity1  = checkEntity1(missedFirstEntity);
-			String activity2  = checkEntity1(missedSecondEntity);
-			
-			Node activityNode1 = graphDB.findNode(NodeType1.activity, "id", activity1);
-			Node activityNode2 = graphDB.findNode(NodeType1.activity, "id", activity2);
-			
-			System.out.println("Display act "+activityNode1.getProperty("id")+ " "+activityNode2.getProperty("id"));
-			
-			String entity1  = checkactivity1(activityNode1);
-			String entity2  = checkactivity1(activityNode2);
-			
-			missedFirstEntity = graphDB.findNode(NodeType1.entity, "id", entity1);
-			missedSecondEntity = graphDB.findNode(NodeType1.entity, "id", entity2);
-			
-			System.out.println("Display act "+missedFirstEntity.getProperty("id")+ " "+missedSecondEntity.getProperty("id"));
-			
-			
-			System.out.println("Comparing Entities "+(String)missedFirstEntity.getProperty("id") +
-					"\t\t"+ (String)missedSecondEntity.getProperty("id"));
-			compare((String) missedFirstEntity.getProperty("attributes"),
-					(String) missedSecondEntity.getProperty("attributes"));
-			
-		} catch (Exception ex) {
-			System.out.println(ex.toString());
+		for (int i = 0, j = 0; i < addVisited.size(); i++, j++) {
+			String DisplayId = addVisited.get(i);
+			String DisplayId2 = addVisited1.get(j);
+			try {
+				ResourceIterator<Node> missedentity1 = graphDB.findNodes(
+						NodeType1.entity, "id", DisplayId);
+				ResourceIterator<Node> missedentity2 = graphDB.findNodes(
+						NodeType1.entity, "id", DisplayId2);
+				Node missedFirstEntity = missedentity1.next();
+				Node missedSecondEntity = missedentity2.next();
+				System.out.println("Comparing Entities "
+						+ (String) missedFirstEntity.getProperty("id") + "\t\t"
+						+ (String) missedSecondEntity.getProperty("id"));
+				compare((String) missedFirstEntity.getProperty("attributes"),
+						(String) missedSecondEntity.getProperty("attributes"));
+				String activity1 = null;
+				String activity2 = null;
+				do {
+					activity1 = checkEntity1(missedFirstEntity);
+					activity2 = checkEntity1(missedSecondEntity);
+
+					Node activityNode1 = graphDB.findNode(NodeType1.activity,
+							"id", activity1);
+					Node activityNode2 = graphDB.findNode(NodeType1.activity,
+							"id", activity2);
+
+					System.out.println("Display act "
+							+ activityNode1.getProperty("id") + " "
+							+ activityNode2.getProperty("id"));
+
+					String entity1 = checkactivity1(activityNode1);
+					String entity2 = checkactivity1(activityNode2);
+
+					missedFirstEntity = graphDB.findNode(NodeType1.entity,
+							"id", entity1);
+					missedSecondEntity = graphDB.findNode(NodeType1.entity,
+							"id", entity2);
+					System.out.println("Comparing Entities "
+							+ (String) missedFirstEntity.getProperty("id")
+							+ "\t\t"
+							+ (String) missedSecondEntity.getProperty("id"));
+					compare((String) missedFirstEntity
+							.getProperty("attributes"),
+							(String) missedSecondEntity
+									.getProperty("attributes"));
+				} while (activity1 == null && activity2 == null);
+			} catch (Exception ex) {
+				System.out.println(ex.toString());
+			}
 		}
 	}
 
 	private static String checkEntity(Node entity1) {
 		String activityNode = null;
-		// TODO Auto-generated method stub
 		TraversalDescription activity1 = graphDB.traversalDescription()
 				.breadthFirst().expand(new PathExpander<Object>() {
 					@Override
@@ -202,7 +190,6 @@ public class TryFT {
 
 					@Override
 					public PathExpander<Object> reverse() {
-						// not used for unidirectional traversals
 						throw new UnsupportedOperationException();
 					}
 				}).evaluator(Evaluators.atDepth(1));
@@ -215,9 +202,9 @@ public class TryFT {
 		}
 		return activityNode;
 	}
+
 	private static String checkEntity1(Node entity1) {
 		String activityNode = null;
-		// TODO Auto-generated method stub
 		TraversalDescription activity1 = graphDB.traversalDescription()
 				.breadthFirst().expand(new PathExpander<Object>() {
 					@Override
@@ -226,11 +213,11 @@ public class TryFT {
 						switch (path.length()) {
 						case 0:
 							return path.endNode().getRelationships(
-									DynamicRelationshipType.withName("wasGeneratedBy"));
+									DynamicRelationshipType
+											.withName("wasGeneratedBy"));
 						case 1:
 							return path.endNode().getRelationships(
-									DynamicRelationshipType
-											.withName("used"));
+									DynamicRelationshipType.withName("used"));
 						default:
 							return Iterables.empty();
 						}
@@ -238,7 +225,6 @@ public class TryFT {
 
 					@Override
 					public PathExpander<Object> reverse() {
-						// not used for unidirectional traversals
 						throw new UnsupportedOperationException();
 					}
 				}).evaluator(Evaluators.atDepth(1));
@@ -254,7 +240,6 @@ public class TryFT {
 
 	private static String checkactivity(Node activity) {
 		String entityNode = null;
-		// TODO Auto-generated method stub
 		TraversalDescription entity1 = graphDB.traversalDescription()
 				.breadthFirst().expand(new PathExpander<Object>() {
 					@Override
@@ -287,10 +272,9 @@ public class TryFT {
 		}
 		return entityNode;
 	}
-	
+
 	private static String checkactivity1(Node activity) {
 		String entityNode = null;
-		// TODO Auto-generated method stub
 		TraversalDescription entity1 = graphDB.traversalDescription()
 				.breadthFirst().expand(new PathExpander<Object>() {
 					@Override
@@ -299,11 +283,11 @@ public class TryFT {
 						switch (path.length()) {
 						case 0:
 							return path.endNode().getRelationships(
-									DynamicRelationshipType
-											.withName("used"));
+									DynamicRelationshipType.withName("used"));
 						case 1:
 							return path.endNode().getRelationships(
-									DynamicRelationshipType.withName("wasGeneratedBy"));
+									DynamicRelationshipType
+											.withName("wasGeneratedBy"));
 						default:
 							return Iterables.empty();
 						}
@@ -339,18 +323,12 @@ public class TryFT {
 
 	private static void checkFirstEntityEqual(String EntityAttributes1,
 			String EntityAttributes2) {
-		// TODO Auto-generated method stub
-
-		// TODO Auto-generated method stub
 		String checkString = "'prov:label'";
 		if (EntityAttributes1.toString().contains(checkString)
 				&& EntityAttributes1.toString().contains(checkString)) {
-			// System.out.println("If Entity Attributes has prov:label");
 			int provLabel1 = EntityAttributes1.indexOf("'prov:label'");
-			// System.out.println("starting index:" + provLabel);
 			int provType1 = EntityAttributes1.indexOf("'prov:type'");
 			int provLabel2 = EntityAttributes2.indexOf("'prov:label'");
-			// System.out.println("starting index:" + provLabel);
 			int provType2 = EntityAttributes2.indexOf("'prov:type'");
 			String fileLabel1 = EntityAttributes1.substring(provLabel1,
 					provType1);
@@ -365,18 +343,12 @@ public class TryFT {
 
 	private static void checkFurtherEntityEqual(String EntityAttributes1,
 			String EntityAttributes2) {
-		// TODO Auto-generated method stub
-
-		// TODO Auto-generated method stub
 		String checkString1 = "'esc:hashvalue'";
 		if (EntityAttributes1.toString().contains(checkString1)
 				&& EntityAttributes2.toString().contains(checkString1)) {
-			// System.out.println("If Entity Attributes has prov:label");
 			int provLabel1 = EntityAttributes1.indexOf("'esc:hashvalue'");
-			// System.out.println("starting index:" + provLabel);
 			int provType1 = EntityAttributes1.indexOf("'prov:type'");
 			int provLabel2 = EntityAttributes2.indexOf("'esc:hashvalue'");
-			// System.out.println("starting index:" + provLabel);
 			int provType2 = EntityAttributes2.indexOf("'prov:type'");
 			String fileLabel1 = EntityAttributes1.substring(provLabel1,
 					provType1);
